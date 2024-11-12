2215f40c3b7fee70ae484051a2bb5889a15e2e3c
Gabbocnc <cianci.gabriele@hotmail.it>
2024-11-12 14:44:49 +0100

    complete task

--- src/compenents/AppFooter.jsx
+++ src/compenents/AppFooter.jsx
@@ -1,5 +1,8 @@
-/* export default function AppFooter() {
+export default function AppFooter() {
     return (
-      
+        <footer className="container">
+            <div> &copy; {new Date().getFullYear()} MyBlog </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nulla voluptatum assumenda. Modi vitae magnam illo, doloribus atque et officia.</p>
+        </footer>
     )
-} */
+} 
